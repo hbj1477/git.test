27b391357ea2760681a66959311aa78fdc59b58f
hbj1477 <85440899+hbj1477@users.noreply.github.com>
2021-06-09 18:01:35 +0800

    github commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello hot-fix chongtu");
         System.out.println("hello hot-fix chongtuaaa");
         System.out.println("hello  chongtuaaa");
+         System.out.println("hello  chongtuaaa");
     }
 }
