95f92668a86644fc54aa592d589285965a1276e9
Layne <Layne@atguigu.com>
2021-06-09 17:56:37 +0800

    github commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello hot-fix chongtu");
         System.out.println("hello hot-fix chongtuaaa");
+        System.out.println("hello  chongtuaaa");
     }
 }
